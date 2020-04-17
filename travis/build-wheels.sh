59b115f5af6cfc3d95ab9efc63ca24498603729c
bwoodsend <bwoodsend@gmail.com>
2020-04-18 00:20:38 +0100

    Actually worked out how to use docker

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -4,16 +4,18 @@ set -e -x
 pwd
 ls
 
-# Compile wheels
-#for PYBIN in /opt/python/*/bin; do
-#    "${PYBIN}/pip" install setuptools bdist_wheel
-#    (cd /io/ && "${PYBIN}/python" setup.py bdist_wheel)
-#done
+# Test then Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" install setuptools wheel auditwheel
+    (cd /io/ && "${PYBIN}/python" setup.py test)
+    (cd /io/ && "${PYBIN}/python" setup.py bdist_wheel)
+done
 
-
-# Install packages and test
-#for PYBIN in /opt/python/*/bin/; do
-#    "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
-#    (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
-#done
+# Wheels aren't considered manylinux unless they have been through 
+# auditwheel. (Know idea why.) Auditted wheels go in /io/wheels/.
+mkdir -p /io/wheels/
+for whl in /io/dist/*.whl; do
+    auditwheel repair "$whl" --plat $PLAT -w /io/wheels/
+        
+done
 
