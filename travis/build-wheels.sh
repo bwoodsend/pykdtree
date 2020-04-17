9676237aca3a1414ad93512f605871b46df12b92
bwoodsend <30940778+bwoodsend@users.noreply.github.com>
2020-04-17 15:59:35 +0100

    Update build-wheels.sh

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -5,11 +5,13 @@ set -e -x
 yum install -y atlas-devel
 
 mkdir -pv /io/wheelhouse
+ls /io/
+pwd
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/pip" install -r /io/dev-requirements.txt
-    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+    "${PYBIN}/pip" install setuptools bdist_wheel
+    "${PYBIN}/python" wheel /io/ -w wheelhouse/
 done
 
 # Bundle external shared libraries into the wheels
