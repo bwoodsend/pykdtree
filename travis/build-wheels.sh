fef6b0450730da8f014f221b01d48a8052fae524
bwoodsend <30940778+bwoodsend@users.noreply.github.com>
2020-04-17 15:53:27 +0100

    Update build-wheels.sh

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -4,7 +4,7 @@ set -e -x
 # Install a system package required by our library
 yum install -y atlas-devel
 
-mkdir -pv wheelhouse
+mkdir -pv /io/wheelhouse
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
