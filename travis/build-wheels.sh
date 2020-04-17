5ad80ff22f2bd3384095c68fbabfed50e389d0d5
bwoodsend <30940778+bwoodsend@users.noreply.github.com>
2020-04-17 16:09:36 +0100

    Update build-wheels.sh

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -1,27 +1,16 @@
 #!/bin/bash
 set -e -x
 
-# Install a system package required by our library
-yum install -y atlas-devel
-
-mkdir -pv /io/wheelhouse
-ls /io/
-pwd
-
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
     "${PYBIN}/pip" install setuptools bdist_wheel
-    "${PYBIN}/python" wheel /io/ -w wheelhouse/
+    (cd /io/ && "${PYBIN}/python" setup.py bdist_wheel)
 done
 
-# Bundle external shared libraries into the wheels
-for whl in wheelhouse/*.whl; do
-    auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
-done
 
 # Install packages and test
-for PYBIN in /opt/python/*/bin/; do
-    "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
-    (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
-done
+#for PYBIN in /opt/python/*/bin/; do
+#    "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
+#    (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
+#done
 
